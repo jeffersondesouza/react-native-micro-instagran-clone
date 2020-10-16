6101daf1b6c1074cfbf80c32547bf3f9982b0a2f
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-10-16 07:36:11 -0300

    load init ok

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+
+/* /usr/local/android-studio/bin/studio.sh  */
+/* 
+sudo chmod 777 -R /dev/kvm
+ */
--- src/api/login.js
+++ src/api/login.js
@@ -1,12 +1,12 @@
 const login = async ({ userName, password }) => {
-  const res = await fetch("http://10.0.2.2:3030/users/login", {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     method: "POST",
     body: JSON.stringify({ userName, password }),
     headers: { "Content-type": "application/json" }
   });
 
   if (res.ok) {
-    const token = await res.headers.get("x-access-token");
+    const token = 'token12345';
     return token;
   } else {
     throw new Error("Não foi possível logar");
