148cf2920f83c78116017ab9cb9e85e8cb10c2eb
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2020-10-24 07:21:03 -0300

    register ok

--- src/view/pages/SingIn/components/Footer/Footer.tsx
+++ src/view/pages/SingIn/components/Footer/Footer.tsx
@@ -4,30 +4,69 @@ import { TextInput, Switch } from "react-native-paper";
 
 const style = StyleSheet.create({
   root: {
-    alignItems: 'center'
-  },
-  cta: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
     flexDirection: 'row',
+    paddingLeft: 20,
+    paddingRight: 20
   },
   ctaLink: {
     marginLeft: 5,
+    textAlign: 'center',
     color: "#18c64e",
   },
-  link: {
+  noRegisterLink: {
     color: "blue",
+    textAlign: 'center',
   },
   text: {
     color: "#686868",
+  },
+  separatorBlock: {
+    alignItems: 'center',
+    flexGrow: 1,
+  },
+  registerBlock: {
+    alignItems: 'center',
+    width: 100
+  },
+  noRegisterBlock: {
+    alignItems: 'center',
+    textAlign: 'right',
+    width: 100
   },
   separator: {
     color: "#686868",
     marginBottom: 10,
     marginTop: 10,
+    textTransform:'uppercase',
+    fontSize: 16,
   },
 });
 
 const Footer = () => {
   return (
+    <View style={style.root}>
+      <View style={style.registerBlock}>
+        <TouchableOpacity>
+          <Text style={style.ctaLink}>Cadastre-se</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={style.separatorBlock}>
+        <Text style={style.separator}>ou</Text>
+      </View>
+      <View style={style.noRegisterBlock}>
+        <TouchableOpacity>
+          <Text style={style.noRegisterLink}>Acesse sem cadastro</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Footer;
+
+/* 
     <View style={style.root}>
       <View style={style.cta}>
         <Text style={style.text}>Novo aqui?</Text>
@@ -42,7 +81,4 @@ const Footer = () => {
         </TouchableOpacity>
       </View>
     </View>
-  );
-};
-
-export default Footer;
+     */
